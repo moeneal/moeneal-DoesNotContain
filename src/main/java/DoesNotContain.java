adcbbed2f8ca1479f0524e95184d34dc1b789d0c
moeneal <momo.mneal@gmail.com>
2023-02-17 01:36:53 +0000

    completed the does not contain challenge

--- src/main/java/DoesNotContain.java
+++ src/main/java/DoesNotContain.java
@@ -15,6 +15,23 @@ public class DoesNotContain {
      * @return true if arr does not contain target. false otherwise.
      */
     public boolean arrayDoesNotContain(int[] arr, int target){
-        return false;
+
+        boolean doesntContain = false;
+
+        for(int i = 0; i < arr.length; i++){
+
+            if(arr[i] != target){
+                doesntContain = true;
+            }
+            else if(arr[i] == target){
+                doesntContain = false;
+                break;
+            }
+
+           
+        }
+       
+       
+        return doesntContain;
     }
 }
